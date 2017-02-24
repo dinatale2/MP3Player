aac74bf31b9f459af8d66a0fe22253e7f3fc0018
Giuseppe Di Natale <guss80@gmail.com>
2017-02-23 22:26:03 -0800

    Introduce a playlist
    Provide playlist functionality.
    
    Also, introduce a volume control.
    
    Signed-off-by: Giuseppe Di Natale <guss80@gmail.com>

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,15 +31,20 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.playlistBox = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.volumeControl = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeControl)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 129);
+            this.button1.Location = new System.Drawing.Point(62, 256);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(49, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +52,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(94, 129);
+            this.button2.Location = new System.Drawing.Point(117, 256);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(49, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,50 +62,93 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(175, 129);
+            this.button3.Location = new System.Drawing.Point(172, 256);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(49, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Pause";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // radioButton1
+            // playlistBox
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(13, 42);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(62, 17);
-            this.radioButton1.TabIndex = 3;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Kalimba";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.playlistBox.FormattingEnabled = true;
+            this.playlistBox.Location = new System.Drawing.Point(12, 12);
+            this.playlistBox.Name = "playlistBox";
+            this.playlistBox.Size = new System.Drawing.Size(262, 199);
+            this.playlistBox.TabIndex = 3;
+            this.playlistBox.DoubleClick += new System.EventHandler(this.playlistBox_DoubleClick);
             // 
-            // radioButton2
+            // button4
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(13, 66);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(52, 17);
-            this.radioButton2.TabIndex = 4;
-            this.radioButton2.Text = "Sleep";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.button4.Location = new System.Drawing.Point(12, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(59, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(76, 217);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(59, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Remove";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(227, 256);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Next";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(8, 256);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Prev";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // volumeControl
+            // 
+            this.volumeControl.Location = new System.Drawing.Point(141, 217);
+            this.volumeControl.Maximum = 100;
+            this.volumeControl.Name = "volumeControl";
+            this.volumeControl.Size = new System.Drawing.Size(133, 45);
+            this.volumeControl.TabIndex = 8;
+            this.volumeControl.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.volumeControl.ValueChanged += new System.EventHandler(this.volumeControl_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 164);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.ClientSize = new System.Drawing.Size(286, 291);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.playlistBox);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.volumeControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "MP3 Player";
+            ((System.ComponentModel.ISupportInitialize)(this.volumeControl)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,8 +159,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ListBox playlistBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TrackBar volumeControl;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace mp3player
 {
     public partial class Form1 : Form
     {
         WMPLib.WindowsMediaPlayer mp3;
-        string file = @"C:\Users\Public\Music\Sample Music\Kalimba.mp3";
         bool paused = false;
+        bool mediaEnded = false;
+        int playingIndex;
 
         public Form1()
         {
             InitializeComponent(); 
             mp3 = new WMPLib.WindowsMediaPlayer();
+            mp3.PlayStateChange += Mp3_PlayStateChange;
+            playingIndex = -1;
+
+            mp3.settings.volume = 30;
+            volumeControl.Value = 30;
+        }
+
+        private void Mp3_PlayStateChange(int NewState)
+        {
+            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsMediaEnded) {
+                mediaEnded = true;
+            }
+
+            if (mediaEnded == true && (WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsTransitioning)
+            {
+                mediaEnded = false;
+
+                if (playlistBox.Items.Count <= 0 || playingIndex + 1 >= playlistBox.Items.Count)
+                {
+                    mp3.controls.stop();
+                    return;
+                }
+
+                // not paused, so load the mp3 and play
+                playingIndex++;
+                mp3.URL = (string)playlistBox.Items[playingIndex];
+                mp3.controls.play();
+                paused = false;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,9 +56,18 @@ namespace mp3player
                 return;
             }
 
+            if (playingIndex < 0)
+            {
+                if (playlistBox.Items.Count == 0)
+                    return;
+
+                playingIndex = 0;
+            }
+
             // not paused, so load the mp3 and play
-            mp3.URL = file;
+            mp3.URL = (string)playlistBox.Items[playingIndex];
             mp3.controls.play();
+            paused = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -51,20 +84,72 @@ namespace mp3player
             paused = true;
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            // choose kalimba
-            file = @"C:\Users\Public\Music\Sample Music\Kalimba.mp3";
-            mp3.controls.stop();
-            paused = false;
+            // add button
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "MP3 Files (*.mp3)|*.mp3";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in fd.FileNames)
+                    playlistBox.Items.Add(file);
+            }
         }
 
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
-            // choose sleep away
-            file = @"C:\Users\Public\Music\Sample Music\Sleep Away.mp3";
-            mp3.controls.stop();
+            // remove button
+            int index = playlistBox.SelectedIndex;
+            if (index >= 0)
+            {
+                playlistBox.Items.RemoveAt(index);
+
+                if (playingIndex >= 0 && index < playingIndex)
+                    playingIndex--;
+            }
+        }
+
+        private void playlistBox_DoubleClick(object sender, EventArgs e)
+        {
+            // double clicking an item on our playlist
+            int index = playlistBox.SelectedIndex;
+            if (index >= 0)
+            {
+                mp3.URL = (string)playlistBox.Items[index];
+                paused = false;
+                mp3.controls.play();
+                playingIndex = index;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // previous button
+            if (playingIndex <= 0)
+                return;
+
+            playingIndex--;
+            mp3.URL = (string)playlistBox.Items[playingIndex];
             paused = false;
+            mp3.controls.play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // next button
+            if (playingIndex >= playlistBox.Items.Count - 1)
+                return;
+
+            playingIndex++;
+            mp3.URL = (string)playlistBox.Items[playingIndex];
+            paused = false;
+            mp3.controls.play();
+        }
+
+        private void volumeControl_ValueChanged(object sender, EventArgs e)
+        {
+            // volume slider
+            mp3.settings.volume = volumeControl.Value;
         }
     }
 }
